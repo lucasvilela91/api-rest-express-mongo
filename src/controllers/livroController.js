79c08816453ed004c1b5264333790e0172683822
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2025-05-05 23:03:28 -0300

    feat: add livrosController and livrosRoutes

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
@@ -22,9 +23,12 @@ class LivroController {
   };
 
   static async cadastrarLivro (req, res) {
+    const novoLivro = await req.body;
     try {
-      const novoLivro = await livro.create(req.body);
-      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+      const autorEncontrado = await autor.findById(novoLivro.autor) 
+      const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc }};
+      const livroCriado = await livro.create(livroCompleto);
+      res.status(201).json({ message: "criado com sucesso", livro: livroCriado });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
     }
@@ -49,6 +53,16 @@ class LivroController {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
     }
   };
+
+  static async listarLivrosPorEditora (req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora)
+    } catch (error) {
+      res.status(500).json({ message: `${erro.message} - falha na busca` });
+    }
+  }
   
 };
 
